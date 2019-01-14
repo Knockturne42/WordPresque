c47237ef9a7a13eac8470f1330ab6f5ee0530f30
dclkent1parker <dcl.quentind@18pixel.fr>
2019-01-14 14:24:06 +0100

    add Quentin

--- Vue/random.php
+++ Vue/random.php
@@ -31,7 +31,25 @@ if(isset($_GET['generer'])) {
 
 			$selectMot->selectDb();
 	}
-	echo $mot['orthMot'];
+	$search = $mot['orthMot'];
+	echo $search;
+	$columnArray = array('idMot', 'orthMot');
+	$valueArray = array('0', '1');
+	// $dbConnectionArray = array('localhost', 'wordpresque', 'test', 'test00');
+	$dbConnectionArray = array('192.168.1.20', 'dcl.nanarchie', 'dcl.nanarchie', 'thixitin');
+	$condition = 'orthMot LIKE "'.$search.'%" ';
+
+	$selectMot = new requete($dbConnectionArray, $columnArray, $valueArray, 'mots', '', $condition);
+	$selectMot->selectDb();
+	$arrayResult = array();
+	while($mot = $selectMot->queryDb->fetch())
+	{
+		array_push($arrayResult, $mot);
+	}
+	$monMot = $arrayResult[rand(0, sizeof($arrayResult)-1)]['orthMot'];
+	$motFinal = '<p class="jeuDeMot">'.substr($_GET['userValue'], 0, -2).'<span>'.$monMot.'</span></p>';
+	echo $motFinal;
+
 }
 
 ?>
